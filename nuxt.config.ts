bdf7c95d0b84f4b2d45577df57f6808b581c8427
Yi-Ru Lan <a78945612385238@gmail.com>
2023-03-08 16:14:09 +0800

    Init

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  runtimeConfig: {
+    NEWEBPAY_MERCHANT_ID: 'ID',
+    NEWEBPAY_HASH_KEY: 'KEY',
+    NEWEBPAY_HASH_IV: 'IV',
+  },
+});
