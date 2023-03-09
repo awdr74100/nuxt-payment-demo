3166de8dddfb3e46343a4dd0e1cc161b301ecb3a
Yi-Ru Lan <a78945612385238@gmail.com>
2023-03-09 15:25:34 +0800

    Use node:crypto instead of crypto-js to encrypt payment data

--- server/api/orders/[id].get.ts
+++ server/api/orders/[id].get.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod';
 import { stringify } from 'node:querystring';
-// import CryptoJS from 'crypto-js';
-// import { createCipheriv, createHash } from 'node:crypto';
+import { createCipheriv, createHash } from 'node:crypto';
 
 export default defineEventHandler(async (event) => {
   try {
@@ -21,7 +20,13 @@ export default defineEventHandler(async (event) => {
 
     const config = useRuntimeConfig();
 
-    const query = stringify({
+    const cipher = createCipheriv(
+      'aes-256-cbc',
+      config.NEWEBPAY_HASH_KEY,
+      config.NEWEBPAY_HASH_IV,
+    );
+
+    const qs = stringify({
       MerchantID: config.NEWEBPAY_MERCHANT_ID,
       RespondType: 'JSON',
       TimeStamp: order.updatedAt,
@@ -32,72 +37,26 @@ export default defineEventHandler(async (event) => {
       Email: order.email,
     });
 
-    const crypto = useCrypto();
+    const TradeInfo = cipher.update(qs, 'utf-8', 'hex') + cipher.final('hex');
 
-    const TradeInfo = crypto.aesEncrypt(query);
-    const TradeSha = crypto
-      .shaEncrypt(
+    const TradeSha = createHash('sha256')
+      .update(
         `HashKey=${config.NEWEBPAY_HASH_KEY}&${TradeInfo}&HashIV=${config.NEWEBPAY_HASH_IV}`,
       )
+      .digest('hex')
       .toUpperCase();
 
-    // const TradeInfo = CryptoJS.AES.encrypt(
-    //   query,
-    //   CryptoJS.enc.Utf8.parse(config.NEWEBPAY_HASH_KEY),
-    //   {
-    //     iv: CryptoJS.enc.Utf8.parse(config.NEWEBPAY_HASH_IV),
-    //     mode: CryptoJS.mode.CBC, // default
-    //     padding: CryptoJS.pad.Pkcs7, // default
-    //   },
-    // ).toString(CryptoJS.format.Hex);
+    /**
+     * Use crypto-js package
+     */
+    // const crypto = useCrypto();
 
-    // const TradeSha = CryptoJS.SHA256(
-    //   `HashKey=${config.NEWEBPAY_HASH_KEY}&${TradeInfo}&HashIV=${config.NEWEBPAY_HASH_IV}`,
-    // )
-    //   .toString(CryptoJS.enc.Hex)
-    //   .toUpperCase();
-
-    // const query = stringify({
-    //   MerchantId: config.NEWEBPAY_MERCHANT_ID,
-    //   RespondType: 'JSON',
-    //   TimeStamp: order.updatedAt,
-    //   Version: '2.0',
-    //   MerchantOrderNo: order.id,
-    //   Amt: order.amount,
-    //   ItemDesc: order.title,
-    // });
-    // console.log('-------------------');
-    // console.log(query);
-
-    // const cipher = createCipheriv(
-    //   'aes-256-cbc',
-    //   config.NEWEBPAY_HASH_KEY,
-    //   config.NEWEBPAY_HASH_IV,
-    // );
-    // const encrypted =
-    //   cipher.update(query, 'utf8', 'base64') + cipher.final('base64');
-
-    // console.log('-------------------');
-    // console.log(encrypted);
-
-    // const hash = createHash('sha256')
-    //   .update(
-    //     `${config.NEWEBPAY_HASH_KEY}&${encrypted}&${config.NEWEBPAY_HASH_IV}`,
+    // const TradeInfo = crypto.aesEncrypt(query);
+    // const TradeSha = crypto
+    //   .shaEncrypt(
+    //     `HashKey=${config.NEWEBPAY_HASH_KEY}&${TradeInfo}&HashIV=${config.NEWEBPAY_HASH_IV}`,
     //   )
-    //   .digest('hex')
     //   .toUpperCase();
-
-    // console.log('-------------------');
-    // console.log(hash);
-
-    // return {
-    //   success: true,
-    //   order,
-    //   TradeInfo: encrypted,
-    //   TradeSha: hash,
-    //   MerchantId: config.NEWEBPAY_MERCHANT_ID,
-    //   Version: '2.0',
-    // };
 
     return {
       success: true,
