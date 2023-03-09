2d3db867b062a779780a506640a57075e84f5aec
Yi-Ru Lan <a78945612385238@gmail.com>
2023-03-09 19:40:07 +0800

    Check NewebPay notify route return data

--- server/api/newebpay/notify.post.ts
+++ server/api/newebpay/notify.post.ts
@@ -1,5 +1,10 @@
 export default defineEventHandler(async (event) => {
   try {
+    console.log(getRouterParams(event));
+    console.log(await readBody(event));
+    console.log(getQuery(event));
+
+    return { success: true };
   } catch (error) {
     return { success: false };
   }
